a2861b743191bf331ecd03cb9da4989aeca45e02
andfian05 <andfian05@gmail.com>
2023-01-04 15:05:09 +0700

    update model mahasantri

--- app/Models/Mahasantri.php
+++ app/Models/Mahasantri.php
@@ -11,7 +11,7 @@ class Mahasantri extends Model
     //mapping ke tabel
     protected $table = 'mahasantri';
     //mapping ke kolom
-    protected $fillable = ['id',',nama','tmp_lahir','tgl_lahir','anak','no_hp','email','alamat'];
+    protected $fillable = ['id', 'nama', 'tmp_lahir', 'tgl_lahir', 'anak', 'no_hp', 'email', 'alamat', 'pendidikan_id', 'keluarga_id', 'jurusan_id', 'informasi_id', 'provinsi_id', 'profil_id', 'berkas_id'];
     //relasi tabel one to one ke tabel pendidikan
     // public function pendidikan()
     // {
@@ -21,37 +21,37 @@ class Mahasantri extends Model
     //merujuk ke tabel mahasantri, jadi data pendidikan di tampilkan ke mahasantri 
     public function pendidikan()
     {
-        return $this->belongsTo(Pendidikan::class);
+        return $this->belongsTo(Pendidikan::class, 'pendidikan_id', 'id');
     }
 
     public function keluarga()
     {
-        return $this->belongsTo(Keluarga::class);
+        return $this->belongsTo(Keluarga::class, 'keluarga_id', 'id');
     }
 
     public function jurusan()
     {
-        return $this->belongsTo(Jurusan::class);
+        return $this->belongsTo(Jurusan::class, 'jurusan_id', 'id');
     }
 
     public function provinsi()
     {
-        return $this->belongsTo(Provinsi::class);
+        return $this->belongsTo(Provinsi::class, 'provinsi_id', 'id');
     }
 
     public function informasi()
     {
-        return $this->belongsTo(Informasi::class);
+        return $this->belongsTo(Informasi::class, 'informasi_id', 'id');
     }
 
     public function profil()
     {
-        return $this->belongsTo(Profil::class);
+        return $this->belongsTo(Profil::class, 'profil_id', 'id');
     }
 
     public function berkas()
     {
-        return $this->belongsTo(Berkas::class);
+        return $this->belongsTo(Berkas::class, 'berkas_id', 'id');
     }
     
 }
