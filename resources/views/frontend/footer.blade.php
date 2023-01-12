e6c734bb3e8df3c57dde6e085fee8b219271a87a
Andika Alifian <111692945+andfian05@users.noreply.github.com>
2023-01-12 16:03:27 +0700

    Add files via upload

---
+++ resources/views/frontend/footer.blade.php
@@ -0,0 +1,56 @@
+<!-- Footer-->
+<footer class="bg-gradient-primary-to-secondary text-center py-3">
+            <div class="container">
+                <div class="row">
+                    <!-- Footer Location-->
+                    <div class="col-lg-4 mb-5 mb-lg-0">
+                        <img src="{{asset('foto/Logo_petikJombang&YBM_PLN.png' )}}" alt="" width="50%" bgcolor="white">
+                        <br/>
+                        <br/>
+                        <h5 class="text-center text-white font-alt mb-4">Pesantren PeTIK Jombang</h5>
+                    </div>
+                    <!-- Footer Social Icons-->
+                    <div class="col-lg-4 mb-5 mb-lg-0">
+                        <h4 class="text-center text-white font-alt mb-4">Social Media</h4>
+                        <div class="flex-row ml-6col-md-8 col-md-offset-2 mb-3">
+                                <a href="https://petik-jombang.vercel.app/" target="_blank"><i class="fa-solid fa-earth-asia fa-2x"></i></a>&nbsp;&nbsp;
+                                <a href="https://www.instagram.com/petik_jombang/" target="_blank"><i class="fa-brands fa-instagram fa-2x"></i></a>&nbsp;&nbsp;
+                                <a href="https://www.youtube.com/@petikjombang4973" target="_blank"><i class="fa-brands fa-youtube fa-2x"></i></a>&nbsp;&nbsp;
+                                <a href="https://www.facebook.com/petikIIjombang/" target="_blank"><i class="fa-brands fa-facebook-f fa-2x"></i></a>
+                          </div>
+                    </div>
+                    <!-- Footer About Text-->
+                    <div class="col-lg-4">
+                        <h4 class="text-center text-white font-alt mb-4">Location</h4>
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3955.465630402716!2d112.21687271380003!3d-7.524082476334461!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e783fab1dc80271%3A0x8c4c1651240d8951!2sPesantren%20PeTIK%20II%20Jombang%20YBM%20PLN!5e0!3m2!1sid!2sid!4v1672806810541!5m2!1sid!2sid" width="100%" height="250" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                        <p class=" mb-0 text-white text-center">
+                        Pesantren PeTIK Jombang - YBM PLN<br/> Jl. KH. Bisri Syansuri RT.01/RW.05, Plosogeneng, Kec. Jombang,Jombang, Jawa Timur 61416.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </footer>
+        <!-- Copyright Section-->
+        <div class="copyright cont py-4 text-center text-black">
+            <div ><small>PMB Pesantren PeTIK Jombang, All rights reserved <i>Express Project</i> </small></div>
+        </div>
+
+        
+        </footer>
+
+       
+
+       
+
+        
+        <!-- Bootstrap core JS-->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="{{asset('assets2/js/scripts.js')}}"></script>
+        <!-- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *-->
+        <!-- * *                               SB Forms JS                               * *-->
+        <!-- * * Activate your form at https://startbootstrap.com/solution/contact-forms * *-->
+        <!-- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *-->
+        <script src="https://cdn.startbootstrap.com/sb-forms-latest.js"></script>
+    </body>
+</html>
