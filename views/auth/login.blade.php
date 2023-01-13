249a3ab4e3be6e607095d3e93d03450e71edf0ad
Andika Alifian <111692945+andfian05@users.noreply.github.com>
2023-01-13 16:13:05 +0700

    Add files via upload

---
+++ views/auth/login.blade.php
@@ -0,0 +1,99 @@
+@extends('layouts.app')
+
+@section('content')
+
+<body class="hold-transition login-page">
+    <div class="container">
+
+
+        <div class="login-box">
+
+            <div class="card card-outline card-primary">
+                <div class="card-header text-center">
+                    <img class="navbar-brand text-center" src="{{asset('foto/Petik_YBM2.png' )}}" height="87px"
+                        width="323px" alt="">
+                </div>
+                <div class="card-body">
+
+                    <div class="content-header text-center">
+                        <div class="container-fluid">
+                            <div class="row mb-2">
+                                <div class="col-sm-12">
+                                    <h1 class="m-0"><b>Masuk Apps</b></h1>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+
+
+
+
+                    <form method="POST" action="{{ route('login') }}">
+                        @csrf
+                        <div class="input-group mb-4">
+
+
+
+
+                            <input id="email" type="email" class="form-control @error('email') is-invalid @enderror"
+                                placeholder="{{ __('Email Akun') }}" name="email" value="{{ old('email') }}" required
+                                autocomplete="email" autofocus>
+
+                            @error('email')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                            @enderror
+
+
+                        </div>
+                        <div class="input-group mb-3">
+                            <input for="password" id="password" type="password"
+                                class="form-control @error('password') is-invalid @enderror"
+                                placeholder="{{ __('Password Akun') }}" name="password" required
+                                autocomplete="current-password">
+
+                            @error('password')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+
+                            </span>
+                            @enderror
+
+
+                        </div>
+                        <div class="content-header text-center">
+                            <div class="container-fluid">
+                                <div class="row mb-2">
+                                    <div class="col-sm-12">
+                                        <button type="submit" class="btn btn-primary btn-block" widh>
+                                            {{ __('Login') }}</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+
+                </div>
+                </form><br>
+
+
+
+            </div>
+        </div>
+    </div>
+    </div>
+
+
+    <script src="{{asset('assets/plugins/jquery/jquery.min.js')}}"></script>
+
+    <script src="{{asset('assets/plugins/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+
+    <script src="{{asset('assets/dist/js/adminlte.min.js')}}"></script>
+</body>
+
+</html>
+<br>
+
+@endsection
---
+++ views/daftar/video.blade.php
@@ -0,0 +1,104 @@
+@extends('daftar.layouts.index')
+@section('content')
+
+<body id="page-top">
+    <!-- Navigation-->
+    <nav class="navbar navbar-expand-lg mb-5 navbar-light fixed-top shadow-sm" id="mainNav">
+        <div class="container px-5">
+            <!-- <img class="navbar-brand" src="{{asset('foto/Petik_YBM.png' )}}" height="70px" width="149px" alt=""> -->
+            <img class="navbar-brand" src="{{asset('foto/Petik_YBM2.png' )}}" height="50px" width="149px" alt="">
+
+            <a class="navbar-brand fw-bold" href="#page-top"></a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive"
+                aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                Menu
+                <i class="bi-list"></i>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarResponsive">
+                <ul class="navbar-nav ms-auto me-4 my-3 my-lg-0">
+
+                </ul>
+                <a class="btn btn-primary bg-gradient-primary-to-secondary  px-3 mb-2 mb-lg-0 rounded-pill"
+                href="https://drive.google.com/file/d/1GJ9UoJ8dFPtS3o0TeStgC8t7JWRLjChl/view" target="_blank"> <i
+                        class="fa-solid fa-cloud-arrow-down"></i>&nbsp; Download Brosur</a>&nbsp;&nbsp;
+                <a class="btn btn-primary bg-gradient-primary-to-secondary  px-3 mb-2 mb-lg-0 rounded-pill"
+                    href="{{ url('/') }}"> <i class="fa-solid fa-bookmark"></i>&nbsp; Panduan Informasi</a>
+
+            </div>
+        </div>
+    </nav>
+    <br><br><br><br><br><br>
+
+
+
+    <div class="container">
+        <div class="card card-info">
+            <div class="card-header">
+                <h3 class="card-title">Informasi Tentang Pesantren PeTIK Jombang</h3>
+            </div>
+            <div class="card-body">
+                <!-- Formulir Pendidikan -->
+                <div class="form-group">
+                <iframe width="100%" height="500" src="https://www.youtube.com/embed/BgNR8w2ZZ1E" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                </div>
+            </div>
+            <div class="card-body">
+                <div class="form-group">
+                    <form action="{{ route('register.storeProfil') }}" id="form1" name="form1" method="POST" id="form1" name="form1" enctype="multipart/form-data">
+                    @csrf
+                        <div class="form-group">
+                            <label for="keterangan">Apakah anda sudah mengetahui gambaran mengenai Pesantren PeTIK
+                                Jombang, dan yakin untuk melakukan pendaftaran?</label>
+                            <div class="form-group -ml-3col-12">
+                                <select id="keterangan" class="custom-select" required="required"
+                                    onclick="tampilkan()"  >
+                                    <option value="">Pilihan</option>
+                                    <option value="sm">Sudah Mengetahui</option>
+                                    <option value="bm">Belum Mengetahui</option>
+                                </select>
+                            </div>
+                        </div>
+                        <!-- Logic Script Menampilkan pesan Saat Salah Satu Option dipilih  -->
+                        <script>
+                            function tampilkan() {
+
+                                var pesan = document.getElementById("form1").keterangan.value;
+                                var p_kontainer = document.getElementById("container");
+
+                                if (pesan == "sm") {
+                                    p_kontainer.innerHTML = `
+                                    <div class="form-group">
+                                        <div class="col-md-12">
+                                            <label for="">Tuliskan apa yang Anda pahami tentang Pesantren PeTIK Jombang :</label>
+                                            <textarea name="keterangan" class="form-control" id="" cols="12" rows="6" required="required"></textarea>
+                                        </div>                   
+                                    </div>
+                                    <div class="form-group">
+                                        <div class="col-12">
+                                            <button class="btn btn-primary" type="submit">Memulai Mengisi</button>
+                                        </div>
+                                    </div>`;
+                                } else if (pesan == "bm") {
+                                    p_kontainer.innerHTML =
+                                        `<p align="center" class="text-danger"><b>Mohon maaf, Anda tidak bisa melanjutkan Pendaftaran.<br/>Anda harus memiliki gambaran mengenai Pesantren PeTIK Jombang sebelum melakukan pendaftaran</b></p>`;
+                                }
+                            }
+
+                        </script>
+                        <div class="container">
+                            <p id="container"></p>
+                        </div>
+                        <div class="form-group row">
+                            <div class="col-12">
+                                <!-- <button name="submit" type="submit" class="btn btn-primary">Selanjutnya&nbsp; <i class="fa-solid fa-circle-chevron-right"></i></button> -->
+                            </div>
+                        </div>
+                    </form>
+
+                </div>
+            </div>
+        </div>
+    </div>
+    <br><br><br><br>
+
+    @endsection
