a822436e27b1fb96b3094cf10e936d934d1cc921
andfian05 <andfian05@gmail.com>
2023-02-04 11:27:13 +0700

    update model survei

--- app/Models/Survei.php
+++ app/Models/Survei.php
@@ -9,7 +9,7 @@ class Survei extends Model
 {
     use HasFactory;
 
-    protected $table = 'Survei';
+    protected $table = 'survei';
     protected $fillable = [
         'mhs_id', 'nama_pewawancara', 'tmp_seleksi', 'lks_seleksi', 'sktm', 'note_sktm', 'progbeasiswa', 'note_progbea', 'izinbeasiswa', 'note_izinbeasiswa', 'statuskel', 'note_statuskel', 'pendkel', 'note_pendkel', 'usiapend', 'note_usiapend', 'kesehatankel', 'note_kesehatankel', 'jenispekrj', 'note_jenispekrj', 'statusrmh', 'note_statusrmh', 'luasrmh', 'note_luasrmh', 'dindingrmh', 'note_dindingrmh', 'ataprmh', 'note_ataprmh', 'lantairmh', 'note_lantairmh', 'alatmsk', 'note_alatmsk', 'perabotan', 'note_perabotan', 'sumberair', 'note_sumberair', 'mck', 'note_mck', 'penerangan', 'note_penerangan', 'lokasirmh', 'note_lokasirmh', 'tataletak', 'note_tataletak', 'miliktanah', 'note_miliktanah', 'harta', 'note_harta'
     ];
